eaf3d178afd08319e5f57932425552dd1497abd6
brosell <bert@rosell.net>
2023-10-30 18:04:59 -0400

    move gpt prompting to a class

--- prompt-web-page.ts
+++ prompt-web-page.ts
@@ -1,36 +1,13 @@
 //https://pub.towardsai.net/engineering-prompt-chains-with-language-models-to-craft-a-summarizer-almighty-web-app-7286de0b0a71
 
-import OpenAI from 'openai';
 import prompts from './prompts';
 import blogs from './testdata/blog';
 import { partition } from './src/utils';
 import { calculateTextSegmentPositions } from './src/text-segmenter';
 import { IContentProvider, WebPageProvider } from './src/web-page-provider';
+import { AiInterface } from './src/ai';
 
-
-const openai = new OpenAI({
-  //apiKey: 'sk-????' //process.env["OPENAI_API_KEY"]
-});
-let askCount = 0;
-const askGpt = async (prompt: string, model: string = 'gpt-3.5-turbo'): Promise<string> => {
-  // console.log('prompt', prompt);
-  try {
-    askCount++;
-    const chatCompletion = await openai.chat.completions.create({
-      messages: [
-        { role: 'system', content: 'you are a very helpful information reviewer.'},
-        { role: 'user', content: prompt }
-      ],
-      model,
-    });
-  
-    return chatCompletion?.choices[0]?.message.content ?? 'FALSE -m';
-
-  } catch (error) {
-    return `FALSE -${error}`;
-  }
-};
-
+const ai = new AiInterface();
 
 (async () => {
   const question = 'how do I promote mentally healthy kids';
@@ -38,7 +15,7 @@ const askGpt = async (prompt: string, model: string = 'gpt-3.5-turbo'): Promise<
 
   const provider: IContentProvider = new WebPageProvider(url);
   const { title, content } = await provider.fetch();
-  const isRelevant = await askGpt(prompts.TITLE_IS_RELEVANT({ question, title }));
+  const isRelevant = await ai.prompt(prompts.TITLE_IS_RELEVANT({ question, title }));
   console.log('relevant title', isRelevant);
   if (isRelevant.startsWith("FALSE")) {
     console.log('article not relevant based on title');
@@ -55,12 +32,11 @@ const askGpt = async (prompt: string, model: string = 'gpt-3.5-turbo'): Promise<
 
   if(highlights) {
     const finalPrompt = prompts.ANSWER({question, chunk:highlights});
-    const finalAnswer = await askGpt(finalPrompt);
+    const finalAnswer = await ai.prompt(finalPrompt);
     console.log('\n\n----\n', finalAnswer);
   } else {
     console.log('no help');
   }
-  console.log('askCount', askCount);
 })();
 
 
@@ -69,7 +45,7 @@ async function reducingPrompt(text: any, promptFn:any) {
   const chunkLocs = calculateTextSegmentPositions(text, 512, 20);
   const promises = chunkLocs
     .map(loc => text.substring(loc.start, loc.end))
-    .map(chunk => askGpt(promptFn(chunk)));
+    .map(chunk => ai.prompt(promptFn(chunk)));
 
   const hitsAndMisses = await Promise.all(promises);
   return hitsAndMisses;
